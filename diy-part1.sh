69fe40e57f70bb9ecb67772b56a717be57113947
jphy1314 <41810119+jphy1314@users.noreply.github.com>
2026-01-21 11:56:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 1. 添加 PassWall 源 (使用 xiaorouji 源，目前最稳定)
-# 注意：使用 >> 追加，不要用 > 覆盖！
-echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' >> feeds.conf.default
-echo 'src-git passwall_packages passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' >> feeds.conf.default
+# 1. 关键步骤：先删除所有包含 passwall 的旧行 (防止重复和格式错误)
+sed -i '/passwall/d' feeds.conf.default
+sed -i '/helloworld/d' feeds.conf.default
 
-# 2. 如果你想用 helloworld (SSR-Plus)，可以解开下面注释，但建议 passwall 和 ssr-plus 二选一
+# 2. 重新添加正确的 PassWall 源 (xiaorouji 源，包含 app 和 packages)
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+
+# 3. (可选) 如果你需要 SSR-Plus，可以使用下面这行，但通常 PassWall 足够了
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
