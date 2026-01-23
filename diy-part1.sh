80c92c4bb9123558f3a941b769ea8092f7af1721
jphy1314 <41810119+jphy1314@users.noreply.github.com>
2026-01-24 02:11:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 1. 关键步骤：先删除所有包含 passwall 的旧行 (防止重复和格式错误)
+# 1. 移除可能存在的冲突源 (清理 Lean 源码中可能自带的旧 Passwall 引用)
 sed -i '/passwall/d' feeds.conf.default
 sed -i '/helloworld/d' feeds.conf.default
 
-# 2. 重新添加正确的 PassWall 源 (包含 app 和 packages)
-echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
+# 2. 添加 PassWall 源 (必须两个都加：packages 是依赖，passwall 是主程序)
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
-
-# 3. (可选) 如果你需要 SSR-Plus，可以使用下面这行，但通常 PassWall 足够了
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
